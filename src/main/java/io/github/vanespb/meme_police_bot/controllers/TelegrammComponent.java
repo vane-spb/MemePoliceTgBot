5faec4aeea5bf1233191c167cd022a01e018a5b3
Нилов Иван Сергеевич <Ivan.Nilov@russianpost.ru>
2020-11-08 23:20:56 +0300

    actual functionality

--- src/main/java/io/github/vanespb/meme_police_bot/controllers/TelegrammComponent.java
+++ src/main/java/io/github/vanespb/meme_police_bot/controllers/TelegrammComponent.java
@@ -1,23 +1,33 @@
 package io.github.vanespb.meme_police_bot.controllers;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.meta.api.methods.send.SendVideo;
+import org.telegram.telegrambots.meta.api.methods.send.*;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.media.InputMedia;
+import org.telegram.telegrambots.meta.api.objects.media.InputMediaPhoto;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.File;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TelegrammComponent extends TelegramLongPollingBot {
     static {
         ApiContextInitializer.init();
@@ -49,6 +59,7 @@ public class TelegrammComponent extends TelegramLongPollingBot {
         try {
             execute(new SendMessage()
                     .setChatId(channelId)
+                    .enableMarkdownV2(true)
                     .setText(message));
         } catch (TelegramApiException e) {
             e.printStackTrace();
@@ -59,7 +70,8 @@ public class TelegrammComponent extends TelegramLongPollingBot {
         SendPhoto sendPhoto = new SendPhoto();
         sendPhoto.setPhoto("meme.jpg", file);
         sendPhoto.setCaption(message);
-        sendPhoto.setChatId(channelId);
+        sendPhoto.setParseMode("markdown");
+        sendPhoto.setChatId(channelId);;
         try {
             execute(sendPhoto);
         } catch (TelegramApiException e) {
@@ -74,6 +86,28 @@ public class TelegrammComponent extends TelegramLongPollingBot {
         sendVideo.setChatId(channelId);
         try {
             execute(sendVideo);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMediaGroup(String message, List<URL> fileURLs) {
+        SendMediaGroup sendMediaGroup = new SendMediaGroup();
+        sendMediaGroup.setChatId(channelId);
+        sendMediaGroup.setMedia(fileURLs.stream().map(url->{
+            InputMedia media = null;
+            if (url.getFile().contains(".jpg")) {
+                media = new InputMediaPhoto();
+            }
+            if (media != null) {
+                media.setParseMode("markdown");
+                media.setCaption(message);
+                media.setMedia(url.toString());
+            }
+            return media;
+        }).collect(Collectors.toList()));
+        try {
+            execute(sendMediaGroup);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
