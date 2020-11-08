0bc5668aef4e687047b669918504074f212c8f17
Нилов Иван Сергеевич <Ivan.Nilov@russianpost.ru>
2020-11-08 23:12:25 +0300

    weekly update

--- src/main/java/io/github/vanespb/meme_police_bot/MemePoliceTgBotApplication.java
+++ src/main/java/io/github/vanespb/meme_police_bot/MemePoliceTgBotApplication.java
@@ -1,7 +1,6 @@
 package io.github.vanespb.meme_police_bot;
 
 import io.github.vanespb.meme_police_bot.controllers.VkComponent;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
 
-@Slf4j
 @EnableScheduling
 @EnableAsync
 @EnableConfigurationProperties
@@ -18,13 +16,9 @@ import org.telegram.telegrambots.ApiContextInitializer;
 public class MemePoliceTgBotApplication {
 
     public static void main(String[] args) {
-        log.info("Step 0");
         ApiContextInitializer.init();
-        log.info("Step 1");
         ConfigurableApplicationContext context = SpringApplication.run(MemePoliceTgBotApplication.class, args);
-        log.info("Step 2");
         VkComponent vkBot = context.getBean(VkComponent.class);
-        log.info("trying to start vk handler");
         vkBot.run();
     }
 
--- src/main/java/io/github/vanespb/meme_police_bot/controllers/VkComponent.java
+++ src/main/java/io/github/vanespb/meme_police_bot/controllers/VkComponent.java
@@ -41,7 +41,6 @@ public class VkComponent extends CallbackApiLongPoll {
     @SneakyThrows
     @Override
     public void run() {
-        System.out.println("vk bot component started");
         super.run();
     }
 
@@ -61,7 +60,7 @@ public class VkComponent extends CallbackApiLongPoll {
             String text = message.getText();
             String telegrammMessageText = text.isEmpty() ?
                     String.format("From %s", author) :
-                    String.format("*%s*%n%s", author, text);
+                    String.format("%s sad in vk conference: %n%s", author, text);
             List<MessageAttachment> attachments = message.getAttachments();
             if (attachments.isEmpty())
                 tgBot.sendMessage(telegrammMessageText);
