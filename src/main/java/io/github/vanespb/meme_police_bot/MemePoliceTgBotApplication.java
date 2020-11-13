8ba1a90f8825fb0ceb50ff4c1c5d7399e367d7f0
Нилов Иван Сергеевич <Ivan.Nilov@russianpost.ru>
2020-11-14 01:03:12 +0300

    ComponentScan off

--- src/main/java/io/github/vanespb/meme_police_bot/MemePoliceTgBotApplication.java
+++ src/main/java/io/github/vanespb/meme_police_bot/MemePoliceTgBotApplication.java
@@ -8,14 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Slf4j
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan
 @EnableConfigurationProperties
 @EnableAsync(proxyTargetClass = true)
 public class MemePoliceTgBotApplication extends SpringBootServletInitializer {
